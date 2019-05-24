459b7d544e50bde18f76b2d1504df94076dc2c3a
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2019-05-24 11:00:57 -0300

    Criando o profile de desenvolvimento

---
+++ src/main/java/com/evandrosantos/cursomc/config/DevelopmentConfig.java
@@ -0,0 +1,29 @@
+package com.evandrosantos.cursomc.config;
+
+import com.evandrosantos.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("development")
+public class DevelopmentConfig {
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAuto;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!ddlAuto.equals("create"))
+            return false;
+
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
